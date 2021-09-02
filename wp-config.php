266cc429078222ee988aa09fca1f8599e41bcacb
Juphet Vitualla <jvitualla@dnamicro.com>
2021-09-02 22:46:13 +0800

    update image

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'doctorspreferred_db2021' );
+define( 'DB_NAME', 'docprep_db2' );
 
 /** MySQL database username */
-define( 'DB_USER', 'doctorspreferred_user' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'sebnmJq8FTJ]' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
